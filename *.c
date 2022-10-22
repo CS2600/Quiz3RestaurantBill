399a04b77032f9d4ed526cfe27656b6658fffe12
jhhocs <joshuaho1@cpp.edu>
2022-10-22 13:19:46 -0700

    Added outline

---
+++ *.c
@@ -0,0 +1,26 @@
+// Write a program that computes the tax and tip on a restaurant bill for a patron. The 
+// program should accept the tax and tip both as percentages as command line arguments. 
+// Values will be passed into the main() function as arguments. Display the meal cost, tax 
+// amount, tip amount, and total bill on the screen. The meal cost should be randomly 
+// chosen between the following four:
+// Salad: $9.95
+// Soup: $4.55
+// Sandwich: $13.25
+// Pizza: $22.35
+
+//Accept tax and tip as command line arguments
+
+//Check for tax and tip in main
+
+//Pick random meal
+
+//Calculate tax amount
+
+//Calculate tip amount
+
+//Calculate total bill
+
+//Print out meal cost, tax amount, tip amount, and total bill
+
+
+
